9002594f8c1a91f9d8d27b11ddb0efa47f72c63a
Michael <mikeatmarvel@gmail.com>
2022-06-02 15:12:15 -0400

    first commit

---
+++ appleMove/GameScene.swift
@@ -0,0 +1,95 @@
+//
+//  GameScene.swift
+//  appleMove
+//
+//  Created by Michael J Antaran on 5/24/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    private var label : SKLabelNode?
+    private var spriteNode : SKSpriteNode?
+    
+    override func sceneDidLoad() {
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -1.62)
+        
+        self.lastUpdateTime = 0
+        
+        // Get label node from scene and store it for use later
+        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        if let label = self.label {
+            label.alpha = 0.0
+            label.run(SKAction.fadeIn(withDuration: 2.0))
+        }
+        
+        // Create shape node to use during mouse interaction
+        let w = (self.size.width + self.size.height) * 0.2
+        self.spriteNode = SKSpriteNode(imageNamed: "SeedBag")
+        self.spriteNode?.name = "SeedBag"
+        self.spriteNode?.size = CGSize(width: w, height: w)
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        if let n = self.spriteNode?.copy() as! SKSpriteNode? {
+            n.position = pos
+            self.addChild(n)
+        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        self.childNode(withName: "SeedBag")?.position = pos
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        self.removeChildren(in: [self.childNode(withName: "SeedBag")] as? [SKNode] ?? [])
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let label = self.label {
+            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        }
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
+    }
+}
